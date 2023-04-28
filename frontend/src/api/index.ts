a64540706bef80ee53c2e1ef42742b2bf5273d9a
Anton Piliugin <anton.piliugin@icloud.com>
2023-04-28 05:23:47 +0500

    API base URL

--- frontend/src/api/index.ts
+++ frontend/src/api/index.ts
@@ -7,7 +7,7 @@ import { FieldValidationError } from '../../../backend/node_modules/express-vali
 import { UserWithoutPassword } from '../../../backend/jwt'
 import { ExtendedCategory } from '../store/application'
 
-const baseURL = process.env.BASE_URL || 'http://localhost:3000'
+const baseURL = process.env.API_BASE_URL || 'http://localhost:3000'
 
 export interface SignupForm {
     email: string
