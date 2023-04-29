546e331ea2a0eae0050f1a1e0329fe62c55aa8f9
Anton Piliugin <anton.piliugin@icloud.com>
2023-04-29 06:20:49 +0500

    Cloud storage

--- backend/CloudinaryStorage.ts
+++ backend/CloudinaryStorage.ts
@@ -12,26 +12,26 @@ cloudinary.config({
 })
 
 function CloudinaryStorage(this: any): void {
-  
+
 }
 
 export const cloudImageDelete = (image: string, cb?: (error: Error | null) => void) => cloudinary.uploader.destroy(image, { resource_type: 'image', invalidate: true }).then(() => cb && cb(null)).catch((err) => cb && cb(err))
 
-CloudinaryStorage.prototype._handleFile = function _handleFile (_req: Request, file: Express.Multer.File, cb: (error?: any, info?: Partial<Express.Multer.File>) => void) {
+CloudinaryStorage.prototype._handleFile = function _handleFile(_req: Request, file: Express.Multer.File, cb: (error?: any, info?: Partial<Express.Multer.File>) => void) {
   const uploadStream = cloudinary.uploader.upload_stream({ tags: 'images' }, function(err, image) {
     if (err) return cb(err)
 
     const imageData = image as UploadApiResponse
     cb(null, {
-      filename: imageData.public_id,
-      path: imageData.url,
+      filename: imageData.url,
+      path: imageData.public_id,
       size: imageData.bytes
     })
   })
   file.stream.pipe(uploadStream)
 }
 
-CloudinaryStorage.prototype._removeFile = function _removeFile (_req: Request, file: Express.Multer.File, cb: (error: Error | null) => void) {
+CloudinaryStorage.prototype._removeFile = function _removeFile(_req: Request, file: Express.Multer.File, cb: (error: Error | null) => void) {
   cloudImageDelete(file.filename, cb)
 }
 
